fbfe945b87d6f1bb597a2563d7705458537d6ce1
lanyang98 <dzwadm@163.com>
2018-12-04 15:39:03 +0800

    Signed-off-by: lanyang98 <dzwadm@163.com>

---
+++ 123.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+	<title>Today&rsquo;s Date</title>
+
+    </head>
+    <body>
+<h1>123</h1>
+        <p>Today&rsquo;s Date(according to this web server) is 
+        <?php
+        Echo date('l, F jS Y.');
+	?>
+	</p>
+    </body>
+</html>
